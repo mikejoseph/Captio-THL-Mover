b6aa5c09837c628a1d3be766e90664b60318fa64
Mike Joseph <mike.joseph5@gmail.com>
2010-12-23 16:23:12 -0500

    Adding installer... haven't really had time to test, so watch out.

---
+++ postinstall.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+defaults write $HOME/Library/LaunchAgents/net.mode3.CaptioMover.plist ProgramArguments -array /usr/bin/osascript $HOME/Library/Application Support/CaptioTHLMover/captio_thl_mover.applescript
